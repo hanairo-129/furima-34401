3ffbef303e062f3f3311ad287924ef35dc2b162f
hanairo-129 <dream.zanarukando.2001@gmail.com>
2021-03-09 19:51:46 +0900

    ユーザー管理機能、マイグレーションファイルのロールバック機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
 
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :last_name, :first_name, :last_name_reading, :first_name_reading, :nick_name, :birthday])
   end
 end
